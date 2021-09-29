8515b78b9eb6d0a7589ae9c0fd8431fb602d6ec1
Fernanda Maki Hirose <72028645+FernandaMakiHirose@users.noreply.github.com>
2021-09-29 11:03:19 -0300

    Update udp-cliente.py

--- udp-cliente.py
+++ udp-cliente.py
@@ -5,13 +5,13 @@ connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print('Cliente socket criado com sucesso!')
 
 host = 'localhost'
-door = 5433
-message = 'Olá servidor, serenão?'
+porta = 5433
+mensagem = 'Olá servidor, serenão?'
 
 try:
 
-    print(f'Cliente: {message}')
-    connection.sendto(message.encode(), (host, 5432))
+    print(f'Cliente: {mensagem}')
+    connection.sendto(mensagem.encode(), (host, 5432))
 
     data, server = connection.recvfrom(4096)
     data = data.decode()
