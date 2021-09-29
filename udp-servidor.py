03381668d3dfdb6f622435f427a3b80419a9b385
Fernanda Maki Hirose <72028645+FernandaMakiHirose@users.noreply.github.com>
2021-09-29 11:06:01 -0300

    Update udp-servidor.py

--- udp-servidor.py
+++ udp-servidor.py
@@ -1,17 +1,17 @@
 import socket
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+conexão = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print('Socket criado com sucesso!')
 
 host = 'localhost'
-door = 5432
+porta = 5432
 
-connection.bind((host, door))
-message = 'Servidor: Olááááá lá lá!!!'
+conexão .bind((host, porta))
+mensagem = 'Servidor: Olááááá lá lá!!!'
 
 while 1:
-    data, address = connection.recvfrom(4096)
+    data, endereço = conexão.recvfrom(4096)
 
 if data:
     print('Servidor enviando mensagem...')
-    connection.sendto(data + (message.encode()), address)
+    conexão.sendto(data + (mensagem.encode()), endereço)
